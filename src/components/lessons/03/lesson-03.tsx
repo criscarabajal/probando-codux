00d7ba1ad8f4f2da5147378464b1c7bd1972ddd4
cristian-carabajal <cris.carabajal93@gmail.com>
2023-01-20 20:27:48 -0300

    probando codux

--- src/components/lessons/03/lesson-03.tsx
+++ src/components/lessons/03/lesson-03.tsx
@@ -15,7 +15,9 @@ export const Lesson03: React.FC = () => {
         <div className={styles.root}>
             <Task />
             <div className={styles.playground}>
-                <div id="top" className={`${styles.eye} ${styles.top}`}></div>
+                <div id="top" className={`${styles.eye} ${styles.top}`}>
+                    <Pupil />
+                </div>
                 <div id="bottom" className={`${styles.eye} ${styles.bottom}`}>
                     <Pupil color={'hotRed'} />
                 </div>
